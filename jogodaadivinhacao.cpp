47897e7d1f5e989db4a084a6a0627de99c22c9c8
Nirlan Souza <nirlan858@gmail.com>
2021-06-04 19:50:02 -0300

    Using cin to store an integer from user keyboard input

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -10,8 +10,22 @@ int main() {
     cout << "* Bem-vindos ao jogo da adivinhação! *"  << endl;
     cout << "**************************************" << endl;
 
-    int numero_secreto = 32;
+    int numero_secreto = 42;
 
-    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
+    int chute;
+    cout << "Qual seu chute? ";
+    cin >> chute;    
+
+    cout << "O valor do seu chute é: " << chute << endl;
+
+    if(chute == numero_secreto) {
+        cout << "Parabéns! Você acertou o número secreto!" << endl;
+    }
+    else if(chute > numero_secreto) {
+        cout << "Seu chute foi maior que o número secreto!" << endl;
+    }
+    else {
+        cout << "Seu chute foi menor que o número secreto!" << endl;
+    }
 
 }
