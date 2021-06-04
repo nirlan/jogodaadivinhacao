42bf82170159510e4193637d9295600b27f6c818
Nirlan Souza <nirlan858@gmail.com>
2021-06-04 19:26:52 -0300

    Including iostream library and adding std namespace

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,13 @@
+#include <locale.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Portuguese");
+    
+    cout << "**************************************" << endl;
+    cout << "* Bem-vindos ao jogo da adivinhação! *"  << endl;
+    cout << "**************************************" << endl;
+
+}
