f365c61ff75b32256edc73299cc8d8dc2cffa749
Nirlan Souza <nirlan858@gmail.com>
2021-06-04 19:36:26 -0300

    Printing strings and variables

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -10,4 +10,8 @@ int main() {
     cout << "* Bem-vindos ao jogo da adivinhação! *"  << endl;
     cout << "**************************************" << endl;
 
+    int numero_secreto = 32;
+
+    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
+
 }
