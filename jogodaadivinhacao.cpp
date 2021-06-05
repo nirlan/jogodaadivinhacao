43f1a52b3113171122e60648f8c44442c5038b9a
Nirlan Souza <nirlan858@gmail.com>
2021-06-04 23:46:28 -0300

    While loop implementation

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -5,30 +5,39 @@ using namespace std;
 int main() {
 
     setlocale(LC_ALL, "Portuguese");
-    
+
     cout << "**************************************" << endl;
     cout << "* Bem-vindos ao jogo da adivinhação! *"  << endl;
     cout << "**************************************" << endl;
 
     const int NUMERO_SECRETO = 42;
 
-    int chute;
-    cout << "Qual seu chute? ";
-    cin >> chute;    
+    bool nao_acertou = true;
+    int tentativas = 0;
 
-    cout << "O valor do seu chute é: " << chute << endl;
+    while(nao_acertou) {
+        tentativas++;
+        int chute;
+        cout << "Tentativa " << tentativas << endl;
+        cout << "Qual seu chute? ";
+        cin >> chute;
 
-    bool acertou = chute == NUMERO_SECRETO;
-    bool maior = chute > NUMERO_SECRETO;
+        cout << "O valor do seu chute é: " << chute << endl;
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
 
-    if(acertou) {
-        cout << "Parabéns! Você acertou o número secreto!" << endl;
+        if(acertou) {
+            cout << "Parabéns! Você acertou o número secreto!" << endl;
+            nao_acertou = false;
+        }
+        else if(maior) {
+            cout << "Seu chute foi maior que o número secreto!" << endl;
+        }
+        else {
+            cout << "Seu chute foi menor que o número secreto!" << endl;
+        }
     }
-    else if(maior) {
-        cout << "Seu chute foi maior que o número secreto!" << endl;
-    }
-    else {
-        cout << "Seu chute foi menor que o número secreto!" << endl;
-    }
+    cout << "Fim de jogo!" << endl;
+    cout << "Você acertou o número secreto em " << tentativas << " tentativas" << endl;
 
 }
